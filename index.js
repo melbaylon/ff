abc93f7b44bf95fafef8ccc6894be0f9459776ff
melbaylon <melbaylon@gmail.com>
2024-07-06 22:59:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const testDir = './test-dir'
 function getContent(path) {
   return fs.readdirSync(path)
 }
-const contents = getContent(testDir)
-console.log(contents)
+
+function listContent(path) {
+  const contents = getContent(path)
+
+  contents.forEach((content) => {
+    if (fs.statSync(content).isDirectory === true) {
+      console.log("isDir")
+    } else {
+      console.log(content)
+    }
+  });
+  
+ 
+
+  
+}
+
+listContent(testDir)
+// console.log(contents)
 
