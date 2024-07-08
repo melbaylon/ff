1fb574f3ba19e1ede5622e1be76c7a10db673f7b
melbaylon <melbaylon@gmail.com>
2024-07-08 20:29:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import * as fs from 'node:fs';
 
 const testDir = './test-dir'
+// listContent()
+tree()
+
+// 
 
 function getContent(path) {
   return fs.readdirSync(path)
 }
+
 
 function listContent(rootDir = testDir) {
   const contents = getContent(rootDir)
@@ -16,5 +21,10 @@ function listContent(rootDir = testDir) {
 
 }
 
-listContent()
+
+// recursively list contents of a directory
+function tree (path) {
+  //
+}
+
 
